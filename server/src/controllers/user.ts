19a2f2bcb96d08566f26167331b0d466cab14ca3
BastD29 <bastiendaniel9@gmail.com>
2024-08-22 19:36:09 +0200

    update user controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -10,18 +10,23 @@ const createUser = async (req: Request, res: Response) => {
         name,
       },
     });
-    res.json(user);
+    res.status(201).send({ message: "User created with success", user });
   } catch (error) {
     console.error((error as Error).message);
+    res.status(500).send({ message: "Error creating user" });
   }
 };
 
 const getUsers = async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
-    res.json(users);
+    res.status(200).send({
+      message: "Users retrieved successfully",
+      users,
+    });
   } catch (error) {
     console.error((error as Error).message);
+    res.status(500).send({ message: "Error getting users" });
   }
 };
 
@@ -35,21 +40,23 @@ const updateUser = async (req: Request, res: Response) => {
         name,
       },
     });
-    res.json(user);
+    res.status(200).send({ message: "User updated with success", user });
   } catch (error) {
     console.error((error as Error).message);
+    res.status(500).send({ message: "Error updating user" });
   }
 };
 
 const deleteUser = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
-    const user = await prisma.user.delete({
+    await prisma.user.delete({
       where: { id: Number(id) },
     });
-    res.json(user);
+    res.status(200).send({ message: "User deleted with success" });
   } catch (error) {
     console.error((error as Error).message);
+    res.status(500).send({ message: "Error deleting user" });
   }
 };
 
