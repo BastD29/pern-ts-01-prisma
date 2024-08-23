a5027546cdd89c8539999941a94ba424f5f58216
BastD29 <bastiendaniel9@gmail.com>
2024-08-23 10:45:07 +0200

    add user service

--- client/src/constants/endpoints.ts
+++ client/src/constants/endpoints.ts
@@ -1,4 +1,7 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const USERS = "/api/users";
+const CREATE = "/create";
+const UPDATE = "/update";
+const DELETE = "/delete";
 
-export { BASE_URL, USERS };
+export { BASE_URL, USERS, CREATE, UPDATE, DELETE };
---
+++ client/src/services/user.ts
@@ -0,0 +1,65 @@
+import { ApiResponseType, fetcher } from ".";
+import { CREATE, DELETE, UPDATE, USERS } from "../constants/endpoints";
+import { UserType } from "../types/user";
+
+// response types
+
+type GetUsersResponseType = {
+  message: string;
+  users: UserType[];
+};
+
+type CreateUserResponseType = {
+  message: string;
+  user: UserType;
+};
+
+type UpdateUserResponseType = {
+  message: string;
+  user: UserType;
+};
+
+type DeleteUserResponseType = {
+  message: string;
+};
+
+// params types
+
+type CreateUserParamsType = {
+  name: string;
+};
+
+const getUsers = async (): Promise<ApiResponseType<GetUsersResponseType>> =>
+  fetcher({
+    method: "get",
+    url: USERS,
+  });
+
+const createUser = async (
+  body: CreateUserParamsType
+): Promise<ApiResponseType<CreateUserResponseType>> =>
+  fetcher({
+    method: "post",
+    url: `${USERS}${CREATE}`,
+    body: { name: body.name },
+  });
+
+const updateUser = async (
+  id: string,
+  body: CreateUserParamsType
+): Promise<ApiResponseType<UpdateUserResponseType>> =>
+  fetcher({
+    method: "put",
+    url: `${USERS}${UPDATE}${id}`,
+    body: { name: body.name },
+  });
+
+const deleteUser = async (
+  id: string
+): Promise<ApiResponseType<DeleteUserResponseType>> =>
+  fetcher({
+    method: "delete",
+    url: `${USERS}${DELETE}${id}`,
+  });
+
+export { getUsers, createUser, updateUser, deleteUser };
